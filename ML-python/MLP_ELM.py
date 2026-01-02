78fef46549b50d377c0ed0f54b9e7a8dd90ff5c1
Antoni Marczuk <antoni.marczuk123@gmail.com>
2026-01-02 15:28:23 +0100

    s

--- ML-python/MLP_ELM.py
+++ ML-python/MLP_ELM.py
@@ -10,11 +10,11 @@ import matplotlib.pyplot as plt
 # Przygotowanie danych uczących i walidujących.
 
 # Funkcja do aproksymacji
-Fun = lambda x: np.sin(x[0,:]) * np.cos(x[1,:])
+Fun = lambda x: 1000 * np.sin(x[0,:]) * np.cos(x[1,:]) / np.exp(0.01 * (x[0,:] + x[1,:]))
 
-n_inputs = 2 # liczba wejść
-n_hidden = 500 # liczba neuronów ukrytych
-n_outputs = 1 # liczba wyjść
+n_inputs = 2 # liczba wejść (misi być takie jak w Fun)
+n_hidden = 500 # liczba neuronów ukrytych (można swobodnie zmieniać)
+n_outputs = 1 # liczba wyjść (nie zmianiać, bo kod zakłada 1 wyjście)
 
 fi = lambda x: np.tanh(x) # funkcja aktywacji neuronów ukrytych
 
@@ -22,27 +22,31 @@ n_train = 10000 # liczba próbek uczących
 n_val = 3000   # liczba próbek walidujących
 
 # Generowanie próbek uczących i walidujących
-X_min = 0; X_max = 10
+X_min = 0
+X_max = 10
 
 X_train = np.random.uniform(X_min, X_max, (n_inputs, n_train))
 Y_train = Fun(X_train).reshape(n_outputs, n_train)
+
+Y_min = Y_train.min() # minimalna wartość Y w zbiorze uczącym
+Y_max = Y_train.max() # maksymalna wartość Y w zbiorze uczącym
+
 X_train = (X_train - X_min) / (X_max - X_min)  # Przeskalowanie do [0, 1]
+Y_train = (Y_train - Y_min) / (Y_max - Y_min)  # Przeskalowanie do [0, 1]
 
 X_val = np.random.uniform(X_min, X_max, (n_inputs, n_val))
 Y_val = Fun(X_val).reshape(n_outputs, n_val)
+
 X_val = (X_val - X_min) / (X_max - X_min)  # Przeskalowanie do [0, 1]
-
-
-# %% __________________________________________________________________
-# Losowa inicjalizacja wag pierwszej warstwy.
-
-# losowa inicjalizacja wag i biasów
-b1 = np.random.uniform(-10, 10, (n_hidden, 1))
-W1 = np.random.uniform(-10, 10, (n_hidden, n_inputs))
+Y_val = (Y_val - Y_min) / (Y_max - Y_min)  # Przeskalowanie do [0, 1]
 
 
 # %% __________________________________________________________________
 # Uczenie sieci.
+
+# losowa inicjalizacja wag i biasów
+b1 = np.random.uniform(-10, 10, (n_hidden, 1))
+W1 = np.random.uniform(-10, 10, (n_hidden, n_inputs))
 
 lambda_reg = 0.0001  # współczynnik regularizacji
 
