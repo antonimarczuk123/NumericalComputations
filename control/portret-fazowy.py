3bf83ce95d1f9ec9495b5f978833dcab72312597
Antoni Marczuk <antoni.marczuk123@gmail.com>
2026-01-28 10:04:48 +0100

    HH

--- control/portret-fazowy.py
+++ control/portret-fazowy.py
@@ -1,23 +1,28 @@
 # %% ================================================================
-# importy
+# Control engineering: Phase portrait of a discrete-time system
+
 
 import jax
 import jax.numpy as jnp
 import matplotlib.pyplot as plt
 
+
+cpu = jax.devices("cpu")[0]
+gpu = jax.devices("gpu")[0]
+dev = cpu
+
+
 @jax.jit
 def simulation(x0, k):
-
-    A = jnp.array([[0.7, 0.1], [0.0, 0.9]])
-    B = jnp.array([[0.1], [0.05]])
-    C = jnp.array([1.0, 0.1])
-
     # x in R^2, u in R^1, y in R^1
     # x(k+1) = f(x(k), u(k))
     # y(k+1) = g(x(k+1))
     def obj(x, u):
-        x_new = A @ x + B.squeeze() * u
-        y_new = C @ x_new
+        x1, x2 = x
+        x1_new = 0.7 * x1 + 0.1 * x2 + 0.1 * u
+        x2_new = 0.9 * x2 + 0.05 * u
+        x_new = jnp.array([x1_new, x2_new])
+        y_new = 1.0 * x1_new + 0.1 * x2_new
         return y_new, x_new
 
     def step(carry, k):
@@ -34,7 +39,8 @@ def simulation(x0, k):
     
     return run(x0, k)
 
-k = jnp.arange(0, 70)
+
+k = jnp.arange(0, 100000, device=dev)
 
 fig = plt.figure()
 ax1 = fig.add_subplot(211)
@@ -42,7 +48,7 @@ ax2 = fig.add_subplot(212)
 
 for x10 in jnp.linspace(-3, 3, 10):
     for x20 in jnp.linspace(-3, 3, 10):
-        x0 = jnp.array([x10, x20])
+        x0 = jnp.array([x10, x20], device=dev)
         y, x = simulation(x0, k)
         ax1.plot(x[:, 0], x[:, 1])
         ax2.plot(k, y)
